a920dba0c761dd81222d9da36aa4e5c0fafcd32c
tabris233 <604925267@qq.com>
2019-04-14 17:07:57 +0800

    create install script for neovim

---
+++ neovim/install.sh
@@ -0,0 +1,7 @@
+echo "=============================="
+echo "create diretory ~/.config/nvim"
+mkdir -p ~/.config/nvim
+echo "copy neovim config file (init.nvim) to ~/.config/nvim"
+cp ./init.vim ~/.config/nvim/
+echo "load config file END"
+echo "=============================="
